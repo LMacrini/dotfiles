0ec6eb1ec8e40393603b094d7b2dd658d0f7ab40
Lionel Macrini <62901118+LMacrini@users.noreply.github.com>
2024-12-03 09:50:29 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 
 cd /etc/nixos
 
+sudo mkdir backups
+
 sudo curl -L -o nixos.tar.gz "https://github.com/LMacrini/nixos/archive/refs/heads/main.tar.gz"
 sudo gunzip nixos.tar.gz
 sudo tar -xf nixos.tar
@@ -9,6 +11,7 @@ sudo rm nixos.tar
 
 # Extract the section from the source file
 awk '/# Bootloader\./ {print_line=1} print_line && /^[[:space:]]*$/ {print_line=0} print_line {print}' configuration.nix | sudo tee section.tmp > /dev/null
+cp configuration.nix backups/original_config.nix
 
 sudo mv nixos-main/* ./
 sudo mv nixos-main/.config ./
@@ -19,6 +22,7 @@ sudo sed -i "s/lioma/$USER/g" configuration.nix home.nix
 if [[ -z "$(<section.tmp)" ]]; then
   printf "No bootloader found in source file, \e[1;33mplease be careful\e[0m\n"
 else
+  cp configuration.nix backups/regular_bootloader_config.nix
   sudo sed -i '$!s@$@\\@g' section.tmp
   sudo sed -i "/# Bootloader\./,/^[[:space:]]*$/c\\$(<section.tmp)\n" configuration.nix
 
