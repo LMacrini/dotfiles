5f1c0ed37a583e9974d4b6c20b97333ecbf9e64a
Lionel Macrini <62901118+LMacrini@users.noreply.github.com>
2024-12-04 15:21:53 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ cd /etc/nixos
 sudo rm -r backups/*
 sudo mkdir .backups
 sudo mv ./* ./.backups
+sudo mv ./.backups/hardware-configuration.nix ./
 
 sudo curl -L -o nixos.tar.gz "https://github.com/LMacrini/nixos/archive/refs/heads/main.tar.gz"
 sudo gunzip nixos.tar.gz
@@ -14,7 +15,6 @@ sudo rm nixos.tar
 sudo mv nixos-main/* ./
 sudo mv nixos-main/.config ./
 sudo rm -r nixos-main init.sh README.md
-sudo nixos-generate-config
 
 host="${1:-${host:-default}}"
 sudo nixos-rebuild switch --flake "/etc/nixos#$host"
