98a9bb6c68aac81acd2148c093a23c6b8a2d2e1b
Lionel Macrini <62901118+LMacrini@users.noreply.github.com>
2024-12-03 23:14:03 -0500

    fix

--- modules/games/greentimer.nix
+++ modules/games/greentimer.nix
@@ -3,7 +3,7 @@
 		greentimer.enable = lib.mkEnableOption "Enables green timer";
 	};
 
-	config = mkIf config.greentimer.enable {
+	config = lib.mkIf config.greentimer.enable {
 		users.users.lioma.packages = with pkgs; [
 			urn-timer
 		];
