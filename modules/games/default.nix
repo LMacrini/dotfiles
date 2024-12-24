c430c17abaeddaaa581ac434dabdc60942256b3c
Lionel Macrini <62901118+LMacrini@users.noreply.github.com>
2024-12-23 19:22:09 -0500

    add tools for games

--- modules/games/default.nix
+++ modules/games/default.nix
@@ -9,6 +9,7 @@
     ./games.nix
     ./launchers.nix
     ./light-games.nix
+    ./tools.nix
     ./greentimer.nix
   ];
 
@@ -20,6 +21,8 @@
     games.standalone.enable = lib.mkDefault config.games.enable;
     games.launchers.enable = lib.mkDefault config.games.enable;
     games.light.enable = lib.mkDefault config.games.enable;
+
+    games.tools.enable = lib.mkDefault config.games.launchers.enable;
 
     games.greentimer.enable = lib.mkDefault (
       config.games.standalone.enable || config.games.launchers.enable || config.games.light.enable
---
+++ modules/games/tools.nix
@@ -0,0 +1,11 @@
+{pkgs, lib, config, ...}: {
+	options = {
+		games.tools.enable = lib.mkEnableOption "Enables game tools";
+	};
+
+	config = lib.mkIf config.games.tools.enable {
+		users.users.lioma.packages = with pkgs; [
+			blockbench
+		];
+	};
+}
