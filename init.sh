b141b5ecb1e91211b898ef993e397ca8c7376703
Lionel Macrini <62901118+LMacrini@users.noreply.github.com>
2024-12-25 00:28:51 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,14 +7,12 @@ sudo mkdir .backups
 sudo mv ./* ./.backups
 sudo mv ./.backups/hardware-configuration.nix ./
 
-sudo curl -L -o nixos.tar.gz "https://github.com/LMacrini/nixos/archive/refs/heads/main.tar.gz"
-sudo gunzip nixos.tar.gz
-sudo tar -xf nixos.tar
-sudo rm nixos.tar
+nix-shell -p git --run "
+git clone https://github.com/lmacrini/nixos --recursive
+"
 
-sudo mv nixos-main/* ./
-sudo mv nixos-main/.config ./
-sudo rm -r nixos-main init.sh README.md
+mv nixos/* .
+sudo rm -r nixos init.sh README.md
 
 host=${1:-default}
 rebuild_type=${2:-switch}
