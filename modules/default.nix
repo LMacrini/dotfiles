5417de741f45c398d3111cd42dbf72383d8c46f9
Lionel Macrini <62901118+LMacrini@users.noreply.github.com>
2025-01-23 20:22:51 -0500

    add nh and others

--- modules/default.nix
+++ modules/default.nix
@@ -179,8 +179,10 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
+    nh
+    nix-output-monitor
+    nvd
+
     git
     nixfmt-rfc-style
     distrobox
