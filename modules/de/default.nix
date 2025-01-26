e59e7d98a633ddc53faac6212ae55d5abae59e7f
Lionel Macrini <62901118+LMacrini@users.noreply.github.com>
2025-01-26 16:29:55 -0500

    remove hyprland

--- modules/de/default.nix
+++ modules/de/default.nix
@@ -2,11 +2,9 @@
   imports = [
     ./dm.nix
     ./gnome.nix
-    ./hyprland.nix
     ./sway.nix
   ];
   
   de.gnome.enable = lib.mkDefault true;
-  de.hyprland.enable = lib.mkDefault false;
   de.sway.enable = lib.mkDefault false;
 }
