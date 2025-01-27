cc5d302fa6d7f1426037c5d18d6d6856cd717e3e
Lionel Macrini <62901118+LMacrini@users.noreply.github.com>
2025-01-27 09:07:43 -0500

    make init script safer

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -eo pipefail
 
 if [ -d "~/dotfiles" ]; then
   echo "dotfiles directory found, making backup..."
@@ -18,6 +19,7 @@ echo 'nix run github:lmacrini/nvf-config --extra-experimental-features "nix-comm
 chmod +x vim.sh
 
 cat << 'EOF' > hdw.sh
+set -u
 nix-shell -p git --run "
 cp /etc/nixos/hardware-configuration.nix ./hosts/$1
 git add ./hosts/$1/hardware-configuration.nix
@@ -26,11 +28,12 @@ EOF
 chmod +x hdw.sh
 
 cat << 'EOF' > build.sh
+set -u
 nix-shell -p nh nom nvd fira-code-nerdfont --run "
 if nh os boot . -H $1 -- --extra-experimental-features 'nix-command flakes'; then
   echo Build successful, rebooting in 5 seconds...
   sleep 5
-  rm build.sh vim.sh hdw.sh
+  rm -f build.sh vim.sh hdw.sh
   reboot
 else
   echo Build unsuccessful, not rebooting
