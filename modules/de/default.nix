72ec60d00ddea77a510e30e3dfe09b42feb91d51
Lionel Macrini <62901118+LMacrini@users.noreply.github.com>
2025-01-26 20:20:26 -0500

    update de defaults

--- modules/de/default.nix
+++ modules/de/default.nix
@@ -1,4 +1,4 @@
-{lib, ...}: {
+{config, lib, ...}: {
   imports = [
     ./dm.nix
     ./gnome.nix
@@ -6,7 +6,8 @@
     ./budgie.nix
   ];
   
-  de.gnome.enable = lib.mkDefault true;
+  # disable gnome by default if budgie is enabled since they don't work together
+  de.gnome.enable = lib.mkDefault (!config.de.budgie.enable);
   de.sway.enable = lib.mkDefault false;
   de.budgie.enable = lib.mkDefault false;
 }
