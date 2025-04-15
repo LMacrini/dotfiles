486d8f5f6f86c93cd889f9bc2c2ba593daaebcdf
Lionel Macrini <62901118+LMacrini@users.noreply.github.com>
2025-04-14 20:06:48 -0400

    clear screen before fastfetch in bashrc

--- home-manager/lioma/default.nix
+++ home-manager/lioma/default.nix
@@ -318,7 +318,7 @@
       eval -- "$(/run/current-system/sw/bin/starship init bash --print-full-init)"
       source "$(blesh-share)/ble.sh"
 
-      # fortune | cowsay -rC | lolcat
+      clear
       fastfetch
     '';
   };
