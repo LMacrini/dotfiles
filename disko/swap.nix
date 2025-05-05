d50a582abcf419c6f2dccec106fe9637f963f5d8
Lionel Macrini <62901118+LMacrini@users.noreply.github.com>
2025-05-05 14:45:05 -0400

    add disko configuration

---
+++ disko/swap.nix
@@ -0,0 +1,41 @@
+{disk}: {
+  disko.devices = {
+    disk = {
+      main = {
+        device = disk;
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "500M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = ["umask=0077"];
+              };
+            };
+            root = {
+              end = "-1G";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+            swap = {
+              size = "100%";
+              content = {
+                type = "swap";
+                discardPolicy = "both";
+                resumeDevice = true; # resume from hiberation from this device
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
