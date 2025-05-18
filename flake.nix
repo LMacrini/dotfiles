6a8500a03fc4943b47956b8bd1221dd1fb9c7277
Lionel Macrini <62901118+LMacrini@users.noreply.github.com>
2025-05-18 08:58:17 -0400

    switch to home manager release

--- flake.nix
+++ flake.nix
@@ -8,8 +8,7 @@
     flake-utils.url = "github:numtide/flake-utils";
 
     home-manager = {
-      # url = "github:nix-community/home-manager/release-24.11";
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
