82d76a439cf41fea50c0dd9009b7eb39dc9b7948
Lionel Macrini <62901118+LMacrini@users.noreply.github.com>
2025-05-23 17:46:10 -0400

    fix iso script

--- buildiso.sh
+++ buildiso.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nix run github:nix-community/nixos-generators -- --flake .#live -f install-iso -o result &> nom
+nix run github:nix-community/nixos-generators -- --flake .#live -f install-iso -o result |& nom
