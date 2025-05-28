03228a7a74bec75a75e36b03f32b1a1ba317da66
LMacrini <62901118+LMacrini@users.noreply.github.com>
2025-05-27 21:55:05 -0400

    add editor envvar on m2 host

--- hosts/Lionels-MacBook-Air/default.nix
+++ hosts/Lionels-MacBook-Air/default.nix
@@ -9,6 +9,10 @@
     inputs.neovim.packages.aarch64-darwin.default
   ];
 
+  environment.variables = {
+    EDITOR = "vim";
+  };
+
   # Necessary for using flakes on this system.
   # nix.settings = {
   #   experimental-features = "nix-command flakes";
