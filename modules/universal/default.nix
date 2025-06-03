2a316be1180c7194377c808ee5e94113ab98d737
Lionel Macrini <liomacrini@gmail.com>
2025-06-04 00:26:44 +0200

    enable pipes

--- modules/universal/default.nix
+++ modules/universal/default.nix
@@ -20,7 +20,7 @@
     channel.enable = false;
 
     settings = {
-      experimental-features = "nix-command flakes";
+      experimental-features = "nix-command flakes pipe-operators";
       trusted-users = [
         "root"
         "${config.mainUser}"
