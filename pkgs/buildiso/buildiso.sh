90c31794161e21fe3f0663d4c9168f4a59af0d58
LMacrini <62901118+LMacrini@users.noreply.github.com>
2025-06-16 01:33:57 -0400

    make buildiso available on all systems even if it doesn't work

--- pkgs/buildiso/buildiso.sh
+++ pkgs/buildiso/buildiso.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+
+set -e
+if ! command -v nixos-rebuild &> /dev/null; then
+  echo "error: nixos-rebuild not found" >&2
+  exit 1
+fi
+
 variant=${1:-iso-installer}
 nixos-rebuild build-image --flake .#live --image-variant $variant |& nom
