388015564be85a7f11da72d52d18acd04cbbe275
Lionel Macrini <liomacrini@gmail.com>
2025-07-02 11:04:57 +0200

    configure waybar more for niri

--- home-manager/lioma/linux/de/niri/waybar/default.nix
+++ home-manager/lioma/linux/de/niri/waybar/default.nix
@@ -14,5 +14,70 @@
       "pulseaudio"
       "battery"
     ];
+
+    "niri/window" = {
+      format = " {title}";
+      separate-outputs = true;
+
+      rewrite = {
+        "(.*) - YouTube — Ablaze Floorp" = "  $1";
+        " NixOS Search - (.*) — Ablaze Floorp" = " 󱄅 $1";
+        "(.*) — Ablaze Floorp" = "  $1";
+      };
+    };
+
+    "battery" = {
+      format = " {icon} {capacity}% ";
+      format-charging = " 󱐋{icon} {capacity}% ";
+      format-icons = [
+        "󰁺"
+        "󰁻"
+        "󰁼"
+        "󰁽"
+        "󰁾"
+        "󰁿"
+        "󰂀"
+        "󰂁"
+        "󰂂"
+        "󰁹"
+      ];
+    };
+
+    "clock" = {
+      format = "{:%F %H:%M}";
+      interval = 1;
+      tooltip-format = "{:%a %b %d %H:%M:%S %Y}";
+    };
+
+    "idle_inhibitor" = {
+      format = " {icon} ";
+      format-icons = {
+        activated = "";
+        deactivated = "";
+      };
+    };
+
+    "pulseaudio" = {
+      on-click = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+      on-click-right = "pavucontrol";
+
+      format = " {icon} {volume}% ";
+      format-icons = {
+        default = ["󰕿" "󰖀" "󰕾"];
+        default-muted = "󰖁";
+        headphone = "󰋋";
+        headphone-muted = "󰟎";
+        headset = "󰋎";
+        headset-muted = "󰋐";
+        "alsa_output.usb-SteelSeries_Arctis_Nova_3-00.analog-stereo" = "󰋎";
+        "alsa_output.usb-SteelSeries_Arctis_Nova_3-00.analog-stereo-muted" = "󰋐";
+      };
+    };
+
+    "tray" = {
+      show-passive-items = true;
+      spacing = 10;
+      reverse-direction = true;
+    };
   };
 }
