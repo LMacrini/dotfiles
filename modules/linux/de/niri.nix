2fb7d3725116f46a96004f97190bec03dc33a3f0
Lionel Macrini <liomacrini@gmail.com>
2025-07-04 16:41:02 +0200

    use unstable niri

--- modules/linux/de/niri.nix
+++ modules/linux/de/niri.nix
@@ -1,9 +1,16 @@
 {
+  pkgs,
   lib,
   config,
+  inputs,
   ...
 }: {
   config = lib.mkIf (config.de.de == "niri") {
     programs.niri.enable = true;
+    programs.niri.package = pkgs.niri-unstable;
+
+    nixpkgs.overlays = [
+      inputs.niri.overlays.niri
+    ];
   };
 }
