767ba8f8b02749445adfe6a85953cb75bf513201
Lionel Macrini <liomacrini@gmail.com>
2025-07-06 13:51:10 +0200

    add genshin on laptop

--- hosts/lionels-laptop/home.nix
+++ hosts/lionels-laptop/home.nix
@@ -1,3 +1,10 @@
 {...}: {
   programs.niri.settings.input.touchpad.scroll-factor = 0.25;
+
+  services.flatpak.packages = [
+    {
+      appId = "moe.launcher.an-anime-game-launcher";
+      origin = "launcher.moe";
+    }
+  ];
 }
