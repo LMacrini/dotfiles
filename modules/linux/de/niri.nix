a29c6d62d9a1dadf8fa5fce5e5a3d4d722c19e4b
Lionel Macrini <liomacrini@gmail.com>
2025-07-07 13:46:58 +0200

    fix wlogout icons

--- modules/linux/de/niri.nix
+++ modules/linux/de/niri.nix
@@ -9,6 +9,12 @@
     programs.niri.enable = true;
     programs.niri.package = pkgs.niri-unstable;
 
+    # seems to fix wlogout icons
+    # see: https://discourse.nixos.org/t/help-loadin-svgs-in-gtk-css/65542
+    programs.gdk-pixbuf.modulePackages = [
+      pkgs.librsvg
+    ];
+
     nixpkgs.overlays = [
       inputs.niri.overlays.niri
     ];
