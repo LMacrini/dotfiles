112cae77582f973baa06220fd1f398bc46ab4c16
LMacrini <liomacrini@gmail.com>
2025-07-28 12:42:30 -0400

    add monitor config (for work)

--- hosts/lionels-laptop/home.nix
+++ hosts/lionels-laptop/home.nix
@@ -4,4 +4,26 @@
   services.flatpak.packages = [
     "org.gnome.Boxes"
   ];
+
+  programs.niri.settings.outputs = {
+    eDP-1 = {
+      enable = true;
+      focus-at-startup = true;
+      position = {
+        x = 0;
+        y = 0;
+      };
+      scale = 1;
+    };
+
+    DP-3 = {
+      enable = true;
+      focus-at-startup = false;
+      position = {
+        x = 0;
+        y = -1200;
+      };
+      scale = 1;
+    };
+  };
 }
