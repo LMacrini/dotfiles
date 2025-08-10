b7374b86a5b993679de35d6e37840693f0033feb
Seija <seijamail@duck.com>
2025-08-10 16:31:38 -0400

    add marksman

--- home-manager/lioma/universal/default.nix
+++ home-manager/lioma/universal/default.nix
@@ -104,6 +104,7 @@
     helix = {
       enable = true;
       extraPackages = with pkgs; [
+        marksman
         unstable.nil # NOTE: can probably use stable in 25.11, unstable is mostly for pipe operators
       ];
 
