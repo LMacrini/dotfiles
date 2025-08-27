3fdbde747bb1a6b96c091d65ce37fab51f5a77f1
Seija <seijamail@duck.com>
2025-08-27 16:29:18 -0400

    do the same for limine

--- modules/linux/bootloader.nix
+++ modules/linux/bootloader.nix
@@ -36,7 +36,7 @@
           ];
           wallpaperStyle = "streched";
           interface = {
-            branding = "WamOS ${config.system.nixos.release}";
+            branding = "${config.system.nixos.distroName} ${config.system.nixos.release}";
             brandingColor = 6; # number from 0-7 that corresponds to:
             # 0: black, 1: red, 2: green, 3: brown, 4: blue, 5: magenta, 6: cyan, 7: gray
           };
