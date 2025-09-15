b40f9613535741ef8dc3390e70fffc0261c37298
Seija <seijamail@duck.com>
2025-09-15 10:07:02 -0400

    disable specialisation properly

--- hosts/amanojaku/default.nix
+++ hosts/amanojaku/default.nix
@@ -24,30 +24,32 @@
   ssh.enable = true;
 
   specialisation = {
-    Work.configuration = lib.mkIf false {
-      environment.etc.specialisation.text = "Work";
-      system.nixos.tags = ["Work"];
+    Work = lib.mkIf false {
+      configuration = {
+        environment.etc.specialisation.text = "Work";
+        system.nixos.tags = ["Work"];
 
-      sudoInsults.enable = false;
+        sudoInsults.enable = false;
 
-      home-manager.users.lioma = _: {
-        home.packages = with pkgs; [
-          ungoogled-chromium
-        ];
+        home-manager.users.lioma = _: {
+          home.packages = with pkgs; [
+            ungoogled-chromium
+          ];
 
-        programs.kitty.settings = {
-          cursor_trail = lib.mkForce 0;
-        };
+          programs.kitty.settings = {
+            cursor_trail = lib.mkForce 0;
+          };
 
-        programs.niri.settings = {
-          animations.enable = false;
+          programs.niri.settings = {
+            animations.enable = false;
 
-          outputs.DP-4 = {
-            enable = true;
-            focus-at-startup = false;
-            position = {
-              x = -240;
-              y = -1200;
+            outputs.DP-4 = {
+              enable = true;
+              focus-at-startup = false;
+              position = {
+                x = -240;
+                y = -1200;
+              };
             };
           };
         };
