c3ec13d9e6257712d306ce523c12e41287371d0d
Seija <seijamail@duck.com>
2025-10-11 23:24:10 -0400

    use ttl again

--- hosts/DESKTOP-VKFSNVPI/home.nix
+++ hosts/DESKTOP-VKFSNVPI/home.nix
@@ -64,18 +64,7 @@
     zoom-us
   ];
 
-  services.flatpak = {
-    remotes = [
-      {
-        name = "launcher.moe";
-        location = "https://gol.launcher.moe/gol.launcher.moe.flatpakrepo";
-      }
-    ];
-    packages = [
-      {
-        appId = "moe.launcher.an-anime-game-launcher";
-        origin = "launcher.moe";
-      }
-    ];
-  };
+  services.flatpak.packages = [
+    "app.twintaillauncher.ttl"
+  ];
 }
