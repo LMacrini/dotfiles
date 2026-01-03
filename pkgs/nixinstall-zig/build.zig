deea9cebbdf1a552c596bbd63a40a4363367412c
Seija <seijamail@duck.com>
2026-01-03 13:48:35 -0300

    add run step for easier debugging

--- pkgs/nixinstall-zig/build.zig
+++ pkgs/nixinstall-zig/build.zig
@@ -13,5 +13,10 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
+    const run_step = b.step("run", "Run the app");
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+
     b.installArtifact(exe);
 }
