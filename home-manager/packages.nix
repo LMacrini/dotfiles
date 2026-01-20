26d5cf184124823d8c075642bd4095ac92050a92
Seija <seijamail@duck.com>
2026-01-19 19:48:24 -0500

    add gleam to 2 indent in editorconfig

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -41,7 +41,7 @@
         indent_style = "tabs";
       };
 
-      "*.{nix,hs,json,jsonc,json5,yaml,yml,toml}" = {
+      "*.{nix,hs,json,jsonc,json5,yaml,yml,toml,gleam}" = {
         indent_size = 2;
       };
     };
