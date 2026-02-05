c954da5da4c849b9951f15ca4d7d17391b6d501c
Seija <seijamail@duck.com>
2026-02-05 13:09:58 -0500

    make niri settings on amanojaku only exist when niri is enabled

--- hosts/amanojaku/home.nix
+++ hosts/amanojaku/home.nix
@@ -1,20 +1,27 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  lib,
+  cfg,
+  ...
+}:
 {
   home.packages = with pkgs; [
     teams-for-linux
   ];
 
-  programs.niri.settings.input.touchpad.scroll-factor = 0.25;
+  programs.niri.settings = lib.mkIf (cfg.de.de == "niri") {
+    input.touchpad.scroll-factor = 0.25;
 
-  programs.niri.settings.outputs = {
-    eDP-1 = {
-      enable = true;
-      focus-at-startup = true;
-      position = {
-        x = 0;
-        y = 0;
+    outputs = {
+      eDP-1 = {
+        enable = true;
+        focus-at-startup = true;
+        position = {
+          x = 0;
+          y = 0;
+        };
+        scale = 2;
       };
-      scale = 2;
     };
   };
 }
