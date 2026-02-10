94f797c7827b31831bde4ef6b88e810ded33da0f
Seija <seijamail@duck.com>
2026-02-10 15:14:25 -0500

    fix install thing

--- modules/install.nix
+++ modules/install.nix
@@ -1,11 +1,11 @@
-{
+{lib, ...}: {
   perSystem = {
     inputs',
     pkgs,
     ...
   }: {
     packages.install = pkgs.writeShellScriptBin "install" ''
-      ${inputs'.disko.packages.disko-install} --flake $1 --disk main $2
+      exec ${lib.getExe inputs'.disko.packages.disko-install} --write-efi-boot-entries --flake $1 --disk main $2
     '';
   };
 }
