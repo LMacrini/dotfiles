d6b1f2439dee44f473b9c8e1f5096ece08060ace
Seija <seijamail@duck.com>
2026-02-10 18:18:10 -0500

    enable kde plasma for vm

--- modules/hosts/vm/vm.nix
+++ modules/hosts/vm/vm.nix
@@ -14,6 +14,12 @@
       base
     ];
 
+    services.desktopManager.plasma6.enable = true;
+    services.displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+    };
+
     networking.hostName = "vm";
   };
 }
