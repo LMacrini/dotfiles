0151e0cf4df6a4b871b4ec8ec27374501007c230
Seija <seijamail@duck.com>
2026-02-10 21:35:00 -0500

    add fish

---
+++ modules/wrappers/fish.nix
@@ -0,0 +1,25 @@
+{
+  inputs,
+  lib,
+  ...
+}: {
+  perSystem = {pkgs, ...}: let
+    config =
+      pkgs.writeText "config.fish"
+      # fish
+      ''
+        ${lib.getExe pkgs.nix-your-shell} fish | source
+        ${lib.getExe pkgs.zoxide} init fish | source
+        ${lib.getExe pkgs.direnv} hook fish | source
+      '';
+  in {
+    packages.fish = inputs.wrappers.lib.wrapPackage {
+      inherit pkgs;
+      package = pkgs.fish;
+
+      flags = {
+        "-C" = "source ${config}";
+      };
+    };
+  };
+}
