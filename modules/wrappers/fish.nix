f1121b3990b5d0ecefee2022751c554c80553497
Seija <seijamail@duck.com>
2026-02-11 10:19:13 -0500

    add fish theme

--- modules/wrappers/fish.nix
+++ modules/wrappers/fish.nix
@@ -3,16 +3,31 @@
   lib,
   ...
 }: {
-  perSystem = {pkgs, ...}: let
+  perSystem = {
+    pkgs,
+    inputs',
+    ...
+  }: let
+    themeConfig = builtins.readFile "${inputs'.catppuccin.packages.fish}/Catppuccin Macchiato.theme";
+    match = builtins.split "(fish_color_.*?)$" themeConfig;
+    themeConfs = builtins.elemAt (builtins.elemAt match 1) 0;
+    lines = lib.splitString "\n" themeConfs |> builtins.map (s: "set -g ${s}");
+
     config =
       pkgs.writeText "config.fish"
       # fish
       ''
-        ${lib.getExe pkgs.nix-your-shell} fish | source
-        ${lib.getExe pkgs.zoxide} init fish | source
-        ${lib.getExe pkgs.direnv} hook fish | source
+        ${builtins.concatStringsSep "\n" lines}
 
-        alias cd z
+        status is-interactive; and begin
+          ${lib.getExe pkgs.nix-your-shell} fish | source
+          ${lib.getExe pkgs.zoxide} init fish | source
+          ${lib.getExe pkgs.direnv} hook fish | source
+
+          alias cd z
+
+          set fish_greeting
+        end
       '';
   in {
     packages.fish = inputs.wrappers.lib.wrapPackage {
