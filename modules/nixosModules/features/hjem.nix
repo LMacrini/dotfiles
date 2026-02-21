527512296234c08b2608c3d74862f7a37a0a66fe
Seija <seijamail@duck.com>
2026-02-20 23:48:03 -0500

    xdg user dirs

--- modules/nixosModules/features/hjem.nix
+++ modules/nixosModules/features/hjem.nix
@@ -1,10 +1,32 @@
-{inputs, ...}: {
-  flake.aspects.hjem.module = {config, ...}: {
+{
+  inputs,
+  lib,
+  ...
+}: {
+  flake.aspects.hjem.module = {
+    config,
+    pkgs,
+    ...
+  }: {
     imports = [
       inputs.hjem.nixosModules.default
     ];
 
-    hjem = {
+    hjem = let
+      home = config.hjem.users.lioma.directory;
+      dirs = {
+        DOCUMENTS = "${home}/Documents";
+        DOWNLOAD = "${home}/Downloads";
+        GAMES = "${home}/Games";
+        MUSIC = "${home}/Music";
+        PICTURES = "${home}/Pictures";
+        PROJECTS = "${home}/Projects";
+        PUBLICSHARE = "${home}/Public";
+        VIDEOS = "${home}/Videos";
+      };
+
+      bindings = lib.mapAttrs' (k: lib.nameValuePair "XDG_${k}_DIR") dirs;
+    in {
       extraModules = [
         inputs.hjem-rum.hjemModules.default
       ];
@@ -19,6 +41,31 @@
         files.".profile" = {
           executable = true;
           source = config.hjem.users.lioma.environment.loadEnv;
+        };
+
+        packages = [
+          pkgs.xdg-user-dirs
+        ];
+
+        environment.sessionVariables = bindings;
+
+        systemd.services.createXdgUserDirectories = let
+          directoriesList = lib.attrValues dirs;
+          mkdir = dir: ''[[ -L ${dir} ]] || run mkdir -p $VERBOSE_ARG ${dir}'';
+        in {
+          restartTriggers = [
+            directoriesList
+          ];
+          script = lib.concatMapStringsSep "\n" mkdir directoriesList;
+        };
+
+        xdg.config.files = {
+          "user-dirs.dirs" = {
+            generator = lib.generators.toKeyValue {};
+            value = builtins.mapAttrs (_: value: ''"${value}"'') bindings;
+          };
+
+          "user-dirs.conf".text = "enabled=False";
         };
       };
 
