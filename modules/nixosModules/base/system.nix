8ccdfee88f28cb1976873e43f5ebed6a4cdcf6a0
Seija <seijamail@duck.com>
2026-02-21 11:47:21 -0500

    make it seios

--- modules/nixosModules/base/system.nix
+++ modules/nixosModules/base/system.nix
@@ -1,5 +1,12 @@
 {
   flake.nixosModules.base = {
+    system.nixos = {
+      distroId = "seios";
+      distroName = "SeiOS";
+      vendorId = "seios";
+      vendorName = "SeiOS";
+    };
+
     nixpkgs.hostPlatform = "x86_64-linux";
   };
 }
