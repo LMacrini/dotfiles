051bb3bbc43e565fe9a427491be25e44c17d1a58
Seija <seijamail@duck.com>
2026-03-08 21:10:57 -0400

    direnv!

--- modules/nixosModules/features/general.nix
+++ modules/nixosModules/features/general.nix
@@ -3,8 +3,24 @@
     deps = [
       "discord"
       "browser"
+      "hjem"
     ];
 
-    module = {pkgs, ...}: {};
+    module = {pkgs, ...}: {
+      hjem.users.lioma = {
+        rum.programs.direnv.enable = true;
+        xdg.config.files."direnv/direnv.toml" = let
+          tomlFormat = pkgs.formats.toml {};
+        in {
+          source = tomlFormat.generate "direnv.toml" {
+            global = {
+              log_format = "-";
+              log_filter = "^$";
+              warn_timeout = "0s";
+            };
+          };
+        };
+      };
+    };
   };
 }
